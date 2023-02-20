1ba9d46c498ff72516b9ede04e3875a3baa7fb02
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2023-02-20 16:42:35 +0100

    add h1

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const hours = document.getElementById("hour");
 const minutes = document.getElementById("minute");
+const seconds = document.getElementById("second");
+const ampm = document.querySelector(".ampm");
 
 function digitalClock(){
     let h = new Date().getHours();
     let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
 
     if(h >= 12) {
         h = ((h-12) == 0) ? 12 : (h-12);
@@ -13,9 +16,11 @@ function digitalClock(){
 
     h = h < 10 ? '0' + h : h;
     m = m < 10 ? '0' + m : m;
+    s = s < 10 ? '0' + s : s;
 
     hours.innerText = h;
     minutes.innerText = m;
+    seconds.innerText = s;
 }
 
 setInterval(() => {
